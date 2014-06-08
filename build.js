4ea43fe0841763560d233891eef4871257c5dd08
Erick Brower <erick@berecruited.com>
2014-06-08 13:23:02 -0700

    Working lunr indexer and bodyparser plugins

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-var metalsmith = require('metalsmith'),
+var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
     templates = require('metalsmith-templates'),
     permalinks = require('metalsmith-permalinks'),
@@ -9,38 +9,52 @@ var metalsmith = require('metalsmith'),
     lunr = require('lunr'),
     _ = require('lodash');
 
+var bodyParser = function bodyParser(files, metalsmith, done) {
+    var isPost = function isPost(file) {
+        return file.collection === 'posts';
+    };
+    _.each(Object.keys(files), function (file) {
+        if (isPost(files[file])) {
+            var data = files[file];
+            data.body = data.contents.toString();
+            delete files[file];
+            files[file] = data;
+        }
+    });
+    setImmediate(done);
+};
+
 var lunrIndexer = function lunrIndexer(files, metalsmith, done) {
-    var idx = lunr(function() {
+    var idx = lunr(function () {
         this.field('title', { boost: 10 });
         this.field('body');
+        this.ref('path');
     });
     _.each(files, function index(file) {
         if (file.collection === 'posts') {
-            file.body = file.contents.toString();
             idx.add(file);
         }
     });
-    files['data/posts_idx.json'] = { 
-        contents: new Buffer(JSON.stringify(idx)),
+    var serialized = JSON.stringify(idx.toJSON());
+    files['data/posts_idx.json'] = {
+        contents: new Buffer(serialized),
         mode: '0644'
     };
     setImmediate(done);
 };
 
-
 var forge = function forge() {
-    metalsmith(__dirname)
-        .use(lunrIndexer)
+    new Metalsmith(__dirname)
         .metadata({
             headline: 'erickbrower',
             tagline: 'I write code. A lot.',
             author: 'Erick Brower <cerickbrower@gmail.com>',
             description: 'My personal blog. @erickbrower'
         })
+        .source('./src')
+        .destination('./build')
+        .use(bodyParser)
         .use(collections({
-            pages: {
-                pattern: 'content/pages/*.md'
-            },
             posts: {
                 pattern: 'content/posts/*.md',
                 sortBy: 'date',
@@ -49,18 +63,15 @@ var forge = function forge() {
         }))
         .use(markdown())
         .use(templates('handlebars'))
-        .use(permalinks({
-            pattern: ':collection/:title'
-        }))
-        .source('./src')
-        .destination('./build')
+        .use(permalinks(':collection/:title'))
+        .use(lunrIndexer)
         .build();
 };
 
 var parallel = function parallel(callbacks, last) {
     var count = callbacks.length;
-    _.each(callbacks, function(cb) {
-        cb(function() {
+    _.each(callbacks, function (cb) {
+        cb(function () {
             count--;
             if (count === 0) {
                 last();
@@ -72,7 +83,7 @@ var parallel = function parallel(callbacks, last) {
 var partialsDir = path.resolve(__dirname, 'templates', 'partials');
 
 var registerPartialFile = function registerPartialFile(name, filePath, next) {
-    fs.readFile(filePath, function(err, data) {
+    fs.readFile(filePath, function (err, data) {
         Handlebars.registerPartial(name, data.toString());
         next();
     });
@@ -82,7 +93,7 @@ var registerPartialFiles = function registerPartialFiles(files, next) {
     var stage = function stage(file) {
         var name = file.replace(/[w+]*.hbt$/, ''),
             filePath = path.resolve(partialsDir, file);
-        return function(next) {
+        return function (next) {
             registerPartialFile(name, filePath, next);
         };
     };
@@ -99,7 +110,7 @@ var registerHelpers = function registerHelpers() {
     Handlebars.registerHelper('top', function top(collection, options) {
         var out = '',
             newest = _.first(collection, 3);
-        _.each(newest, function(item) {
+        _.each(newest, function (item) {
             out += options.fn(item);
         });
         return out;
